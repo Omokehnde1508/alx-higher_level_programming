bf95b6abaf1651e011e1852fadf4e32de6bf3f90
Adebisi Omokehinde <iimmortalworld003@gmail.com>
2022-12-11 20:44:27 +0100

    errors fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,20 +1,12 @@
-Executable File  19 lines (15 sloc)  468 Bytes
 #!/usr/bin/python3
-#  Prints the number of and the list of its arguments
 if __name__ == "__main__":
     import sys
-
-    arg = sys.argv
-    size = len(arg) - 1
-
-    if size > 1:
-        print("{} arguments:".format(size))
-        for i in range(1, size + 1):
-            print("{}: {}".format(i, arg[i]))
-
-    elif size == 0:
-        print("{} arguments.".format(size))
-
+    if len(sys.argv) == 1:
+        print("0 arguments.")
     else:
-        print("{} argument:".format(size))
-        print("{}: {}".format(size, arg[1]))
+        print("{:d} argument".format(len(sys.argv) - 1), end="")
+        if len(sys.argv) > 2:
+            print("s", end="")
+        print(":")
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {:s}".format(i, sys.argv[i]))

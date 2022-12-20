7b94196c46d08c081dcaba5bd13dec87f0325ace
Adebisi Omokehinde <iimmortalworld003@gmail.com>
2022-12-20 02:16:27 +0100

    taskk 6

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+def raise_exception_msg(message=""):
+    raise NameError(message)
 try:
-    raise_exception_msg("This is a name error")
+  raise_exception_msg("This is a custom message for the exception")
 except NameError as e:
-    print(e)
+  print(e)

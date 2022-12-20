a930ad97e19ea068d7ef6004269a51de850b3cae
Adebisi Omokehinde <iimmortalworld003@gmail.com>
2022-12-20 02:40:05 +0100

    fixed o

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
+    elements_printed = 0
     try:
-        for i in range(x):
-            print(my_list[i], end=" ")
-            count += 1
+        while elements_printed < x:
+            print(my_list[elements_printed], end=' ')
+            elements_printed += 1
     except IndexError:
         pass
     print()
-    return count
+    return elements_printed

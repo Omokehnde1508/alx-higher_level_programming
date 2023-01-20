203874ea08f44791161735f0261b1a5dd6223c62
Adebisi Omokehinde <iimmortalworld003@gmail.com>
2023-01-20 18:11:43 +0100

    task 0 fixed

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+""" no importtation of module"""
+
+
 class Square:
-    """
-    The Square class represents a square shape in a 2-Dimensional space
-    """
+    """The Square class represents a square"""
     def __init__(self):
-        """
-        Initializes an empty square
-        """
+        """Initializes an empty square"""
         pass

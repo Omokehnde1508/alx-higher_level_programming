1d102c8a19d56ffc17ade3be0d379600e1d3e33d
Adebisi Omokehinde <iimmortalworld003@gmail.com>
2023-01-20 18:26:31 +0100

    task 1 done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" no modeules imported"""
+
+
+class Square:
+    """The Square class represents a square shape in a 2-Dimensional space"""
+
+    def __init__(self, size):
+        """
+        Initializes a square with a given sie
+        :param size: the size of the square
+        """
+        self.__size = size

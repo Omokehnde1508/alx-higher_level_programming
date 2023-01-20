af8f988adade186f13bde914ee957e3946b59bdd
Adebisi Omokehinde <iimmortalworld003@gmail.com>
2023-01-20 19:11:25 +0100

    done task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """
+    The Square class represents a square shape in a 2-Dimensional space
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a square with an optional size
+
+        :param size: the size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size

3c88093cde7eaa21d3a9720c9e839a50769f433c
Omokehnde1508 <iimmortalworld003@gmail.com>
2023-02-16 16:34:14 +0100

    áll errors fixed

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,5 +1,4 @@
 -- Removes all records with a score <= 5 in the table second_table of the database
+
 DELETE FROM second_table
-WHERE score <= 5-- Removes all records with a score <= 5 in the table second_table of the database
-DELETE FROM second_table
-WHERE score <= 5
+WHERE score <= 5--
